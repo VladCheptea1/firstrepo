d3f494b7570c9e5bd6fe7f5c916826c58640e504
VladCheptea1 <vladandrei10@yahoo.com>
2019-11-16 12:14:59 +0200

    initial commit

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //int sum = sum();
+            //Console.WriteLine(Sum());
+
+            Sum();
+            int first = 7;
+            int second = 10;
+            Console.WriteLine(Multiply(first, second));
+            DisplayNumbers((100));
+
+        }
+
+        static void Sum()
+        {
+            int a = 2;
+            int b = 3;
+
+            int sum = a + b;
+            Console.WriteLine(sum);
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static void DisplayNumbers(int n)
+        {
+            for (int 1=1; int<=n; int++)
+            {
+
+            }
+
+
+
+        }
+
+    }
+}
